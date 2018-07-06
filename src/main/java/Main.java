b9fdc13c61d2030370189bb694db6d124f186646
Maysam Mokarian <mamokari@microsoft.com>
2018-07-06 13:55:33 -0400

    push the compiter vision code to git

---
+++ src/main/java/Main.java
@@ -0,0 +1,60 @@
+
+import java.net.URI;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+public class Main {
+
+    private static final String subscriptionKey = getKey();
+    private static final String uriBase ="https://eastus.api.cognitive.microsoft.com/vision/v1.0/analyze";
+    private static final String imageToAnalyze =
+            "https://upload.wikimedia.org/wikipedia/commons/1/12/Broadway_and_Times_Square_by_night.jpg";
+
+    public static void main(String[] args) {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        try {
+            URIBuilder builder = new URIBuilder(uriBase);
+            builder.setParameter("visualFeatures", "Categories,Description,Color");
+            builder.setParameter("language", "en");
+
+            // Prepare the URI for the REST API call.
+            URI uri = builder.build();
+            HttpPost request = new HttpPost(uri);
+
+            // Request headers.
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Request body.
+            StringEntity requestEntity =
+                    new StringEntity("{\"url\":\"" + imageToAnalyze + "\"}");
+            request.setEntity(requestEntity);
+
+            // Make the REST API call and get the response entity.
+            HttpResponse response = httpClient.execute(request);
+
+            if (response!=null && response.getEntity() != null) {
+                // Format and display the JSON response.
+                String jsonString = EntityUtils.toString(response.getEntity());
+                JSONObject json = new JSONObject(jsonString);
+                System.out.println("REST Response:\n");
+                System.out.println(json.toString(2));
+            }
+        } catch (Exception e) {
+            // Display error message.
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static String getKey() {
+        return "YOUR_KEY_HERE";
+    }
+}
